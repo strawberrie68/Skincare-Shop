e150cd57ac63babfc0e1cd76e2166638a92e716d
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-08-15 17:37:05 -0700

    nav fix

--- skincare/src/App.tsx
+++ skincare/src/App.tsx
@@ -1,9 +1,8 @@
-import Navbar from "@/scenes/navbar"
-import { useEffect, useState } from "react";
-import { SelectedPage } from "@/shared/types";
+// import { useEffect, useState } from "react";
+// import { SelectedPage } from "@/shared/types";
 import Home from "@/pages/homepage"
 import Quiz from "@/pages/quiz";
-// import Shop from "@/pages/shop"
+import Shop from "@/pages/shop"
 // import Cart from "@/pages/cart"
 
 
@@ -14,33 +13,33 @@ import {
 } from "react-router-dom"
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
-    SelectedPage.Home
-  );
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  // const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+  //   SelectedPage.Home
+  // );
+  // const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.Home);
-      }
-      if (window.scrollY !== 0) setIsTopOfPage(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY === 0) {
+  //       setIsTopOfPage(true);
+  //       setSelectedPage(SelectedPage.Home);
+  //     }
+  //     if (window.scrollY !== 0) setIsTopOfPage(false);
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
 
   return (
    <div className='app bg-gray-20'> 
 
 
     <Router>
-    <Navbar
+    {/* <Navbar
        isTopOfPage={isTopOfPage}
        selectedPage={selectedPage}
        setSelectedPage={setSelectedPage}
-    />
+    /> */}
 
 
         <Routes>
@@ -48,8 +47,8 @@ function App() {
           <Route path="/quiz" element={<Quiz />} />
 
           {/* PAGES TO ADD/ OR IN PROGRESS */}
-          {/* <Route path="/shop" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} /> */}
+          <Route path="/shop" element={<Shop />} />
+          {/* <Route path="/cart" element={<Cart />} /> */}
           
 
         </Routes>
